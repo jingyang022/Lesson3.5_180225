60ded05966e5b432262c9945ff557fecfe68daf0
jingyang022 <jingyang022@yahoo.com.sg>
2025-02-20 13:29:25 +0800

    added files

--- ecs.tf
+++ ecs.tf
@@ -4,64 +4,81 @@ resource "aws_ecs_cluster" "ecs_cluster" {
 }
 
 # Create ECS task definition
-# resource "aws_ecs_task_definition" "service" {
-#   family = "yap-flask-xray-taskdef"
-#   task_role_arn            = aws_iam_role.ecs_task_role.arn
-#   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
-#   network_mode             = "awsvpc"
-#   requires_compatibilities = ["FARGATE"]
-#   cpu                      = 1024
-#   memory                   = 3072
+resource "aws_ecs_task_definition" "flask-xray-taskdef" {
+  family = "yap-flask-xray-taskdef"
+  task_role_arn            = aws_iam_role.ecs_task_role.arn
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = 1024
+  memory                   = 3072
   
-#   container_definitions = jsonencode([
-#     {
-#       name      = "first"
-#       image     = "service-first"
-#       cpu       = 10
-#       memory    = 512
-#       essential = true
-#       portMappings = [
-#         {
-#           containerPort = 80
-#           hostPort      = 80
-#         }
-#       ]
-#     },
-#     {
-#       name      = "second"
-#       image     = "service-second"
-#       cpu       = 10
-#       memory    = 256
-#       essential = true
-#       portMappings = [
-#         {
-#           containerPort = 443
-#           hostPort      = 443
-#         }
-#       ]
-#     }
-#   ])
-# }
+  container_definitions = jsonencode([
+    {
+      name      = "flask-app"
+      image     = "255945442255.dkr.ecr.ap-southeast-1.amazonaws.com/yap-flask-xray-repo:latest"
+      cpu       = 1024
+      memory    = 3072
+      essential = true
+      portMappings = [
+        {
+          containerPort = 8080
+          hostPort      = 8080
+          protocol      = "tcp"
+        }
+      ]
+       environment = [
+        {
+          name  = "MY_APP_CONFIG"
+          value = "arn:aws:ssm:ap-southeast-1:255945442255:parameter/yapapp/config"
+        },
+        {
+          name  = "MY_DB_PASSWORD"
+          value = "arn:aws:secretsmanager:ap-southeast-1:255945442255:secret:yapapp/db_password-VRnUa9"
+        },
+        {
+          name  = "SERVICE_NAME"
+          value = "yap-flask-xray-service"
+        },
+      ]
+    },
+    {
+      name      = "xray-sidecar"
+      image     = "amazon/aws-xray-daemon"
+      cpu       = 1024
+      memory    = 3072
+      essential = false
+      portMappings = [
+        {
+          containerPort = 2000
+          hostPort      = 2000
+          protocol      = "udp"
+        }
+      ]
+    }
+  ])
+}
 
 # Create ECS service
-# resource "aws_ecs_service" "main" {
-#     name            = "cb-service"
-#     cluster         = aws_ecs_cluster.main.id
-#     task_definition = aws_ecs_task_definition.app.arn
-#     desired_count   = var.app_count
-#     launch_type     = "FARGATE"
+resource "aws_ecs_service" "yap-ecs-service" {
+    name            = "yap-flask-service"
+    cluster         = aws_ecs_cluster.ecs_cluster.id
+    task_definition = aws_ecs_task_definition.flask-xray-taskdef.arn
+    desired_count   = 1
+    force_delete    = true
+    launch_type     = "FARGATE"
 
-#     network_configuration {
-#         security_groups  = [aws_security_group.ecs_tasks.id]
-#         subnets          = aws_subnet.private.*.id
-#         assign_public_ip = true
-#     }
+    network_configuration {
+        security_groups  = [aws_security_group.ecs-sg.id]
+        subnets          = data.aws_subnets.public.ids
+        assign_public_ip = true
+    }
 
-#     load_balancer {
-#         target_group_arn = aws_alb_target_group.app.id
-#         container_name   = "cb-app"
-#         container_port   = var.app_port
-#     }
+    # load_balancer {
+    #     target_group_arn = aws_alb_target_group.app.id
+    #     container_name   = "cb-app"
+    #     container_port   = var.app_port
+    # }
 
-#     depends_on = [aws_alb_listener.front_end, aws_iam_role_policy_attachment.ecs-task-execution-role-policy-attachment]
-# }
+    # depends_on = [aws_alb_listener.front_end, aws_iam_role_policy_attachment.ecs-task-execution-role-policy-attachment]
+}
--- ssm-secrets.tf
+++ ssm-secrets.tf
@@ -1,5 +1,5 @@
-resource "aws_ssm_parameter" "yap_parameters" {
-  name  = "/yapapp/config"
-  type  = "String"
-  value = "MySSMConfig"
-}
+# resource "aws_ssm_parameter" "yap_parameters" {
+#   name  = "/yapapp/config"
+#   type  = "String"
+#   value = "MySSMConfig"
+# }
